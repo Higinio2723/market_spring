f2f646c4c74119ef7900dd5a66467b00eecae10b
Higinio Gonzalez <gonzacah23@hotmail.com>
2023-09-20 22:10:02 +0100

    Primer proyecto a realizar microservicio

---
+++ customer/src/main/java/com/paymentchain/customer/repository/CustomerRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/SpringFramework/Repository.java to edit this template
+ */
+package com.paymentchain.customer.repository;
+
+import com.paymentchain.customer.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author hgonzalez
+ */
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    
+    
+    
+}
